d8cd10d008397160a131a0d2aac6a3f67e642ec7
Madelline Butcher <madellinebutcher@gmail.com>
2018-05-10 21:53:40 -0600

    updates

--- app/CarsController.js
+++ app/CarsController.js
@@ -16,9 +16,8 @@ function CarController(){
             <h3>Make: ${car.make}</h3>
             <h3>Model: ${car.model}</h3>
             <h3>Year: ${car.year}</h3>
-            <h3>Price: ${car.price}</h3>
-            <button onclick="app.controllers.carController.discountCar('${car._id}',${car.price})">Discount</button>
-            <p>Description: ${car.description}</p>
+            <h3>Price: ${car.price}</h3> <button onclick="app.controllers.carController.discountCar('${car._id}',${car.price})">Discount</button>
+            <p>${car.description}</p>
             <button onclick="app.controllers.carController.deleteCar('${car._id}')">Delete</button>
           </div>
         ` 
